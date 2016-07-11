cace210f6bf743721f5bfaf3a6a6a5585da7f2b2
Brian Radebaugh <rivukis@gmail.com>
2016-07-11 01:19:22 -0700

    made a simple TDD framework for Xcode playgrounds

---
+++ Testable.swift
@@ -0,0 +1,125 @@
+
+/**
+ # Public Interface
+ ## Adding Test(s)
+ `addTest(description: String, failureMessage: String, _: () -> Bool)`
+ 
+ `faddTest(description: String, failureMessage: String, _: () -> Bool)`
+ 
+ `xaddTest(description: String, failureMessage: String, _: () -> Bool)`
+ 
+ ## Executing Test(s)
+ `executeTests(autoPrintTests: Bool = true)`
+ 
+ # Output Notation
+ "." : Test succeeded
+ 
+ "F" : Test failed
+ 
+ ">" : Test marked as pending
+ */
+public class RIVTester {
+    private struct RIVTest {
+        let description : String
+        let executable : () -> Bool
+        let failureMessage : String
+        let pending : Bool
+        let focused : Bool
+    }
+    
+    private var tests = [RIVTest]()
+    private var hasFocusedTest = false
+    
+    // MARK: Public
+    
+    public init() {}
+    
+    /**
+     Adds a test.
+     
+     - Parameter description:       The description to output if the test is executed.
+     - Parameter failureMessage:    The message to output if the test is executed and fails.
+     - Parameter block:             The block to determine if the test succeeds or fails.
+     */
+    public func addTest(description description: String, failureMessage: String, _ executable: () -> Bool) {
+        tests.append(RIVTest(description: description, executable: executable, failureMessage: failureMessage, pending: false, focused: false))
+    }
+    
+    /**
+     Adds a focused test.
+     
+     - Important: If at one or more focused tests are added, all non-focused tests will be treated as pending.
+     
+     - Parameter description:       The description to output if the test is executed.
+     - Parameter failureMessage:    The message to output if the test is executed and fails.
+     - Parameter block:             The block to determine if the test succeeds or fails.
+     */
+    public func faddTest(description description: String, failureMessage: String, _ executable: () -> Bool) {
+        hasFocusedTest = true
+        tests.append(RIVTest(description: description, executable: executable, failureMessage: failureMessage, pending: false, focused: true))
+    }
+    
+    /**
+     Adds a pending test.
+     
+     - Warning: Pending tests will not be executed.
+     - Note: This has the same function signature as its counterparts for ease of use.
+     
+     - Parameter description:       The description to output if the test is executed.
+     - Parameter failureMessage:    The message to output if the test is executed and fails.
+     - Parameter block:             The block to determine if the test succeeds or fails.
+     */
+    public func xaddTest(description description: String, failureMessage: String, _ executable: () -> Bool) {
+        tests.append(RIVTest(description: description, executable: executable, failureMessage: failureMessage, pending: true, focused: false))
+    }
+    
+    /**
+     Executes the tests.
+     
+     - Parameter autoPrintTests:    Specify if the resulting output should be printed. Defaults to true
+     
+     - Returns: The resulting output `String` that will/would be printed, specified by `autoPrintTests`.
+     */
+    public func executeTests(autoPrintTests shouldPrint: Bool = true) -> String {
+        let result = processTests()
+        
+        if shouldPrint {
+            print(result)
+        }
+        
+        return result
+    }
+    
+    // MARK: Private
+    
+    private func processTests() -> String {
+        var result = ""
+        var succeeded = 0
+        var pending = 0
+        
+        for (index, test) in tests.enumerate() {
+            result += testOutput(test, number: index, succeeded: &succeeded, pending: &pending)
+        }
+        
+        return result + endLine(totalCount: self.tests.count, succeeded: succeeded, pending: pending)
+    }
+    
+    private func endLine(totalCount totalCount: Int, succeeded: Int, pending: Int) -> String {
+        return "\nExecuted \(tests.count) test(s) | \(succeeded) succeeded | \(tests.count - succeeded - pending) failed | \(pending) pending"
+    }
+    
+    private func testOutput(test: RIVTest, number: Int, inout succeeded : Int , inout pending : Int) -> String {
+        let isPending = test.pending || (hasFocusedTest && !test.focused)
+        if isPending {
+            pending += 1
+            return "> Test \(number): \(test.description)\n"
+        }
+        else if test.executable() {
+            succeeded += 1
+            return ". Test \(number): \(test.description)\n"
+        }
+        else {
+            return "F Test \(number): \(test.description) -> \(test.failureMessage)\n"
+        }
+    }
+}
